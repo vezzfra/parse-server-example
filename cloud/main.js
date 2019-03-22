fc6f410d6927b7218210b877e89f7c0273278486
vezzfra <vezzfra@gmail.com>
2019-03-23 00:37:38 +0100

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -3,10 +3,10 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
 
-Parse.Cloud.define('sendNotification', function(code) {
+Parse.Cloud.define('sendNotification', function(req, res) {
   var jsonBody = {
     app_id: "f9b9293b-9e2e-451e-bc78-d6ced90b5742",
-    include_player_ids: [code],
+    include_player_ids: [req.params.code],
     contents: {en: "Nuova attivazione"},
     data: {foo: "bar"}
   };
