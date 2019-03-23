1ca4c425aac409a012dd8933ccec666154232c54
vezzfra <vezzfra@gmail.com>
2019-03-23 10:18:47 +0100

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,30 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define('send', function(req, res) {
+ 
+  var jsonBody = {
+    app_id: "f9b9293b-9e2e-451e-bc78-d6ced90b5742",
+    included_segments: ["All"],
+    contents: {en: "Nuova attivazione"},
+    data: {foo: "bar"}
+  };
+
+  
+  Parse.Cloud.httpRequest({ 
+  url: "https://onesignal.com/api/v1/notifications", 
+  method: "POST", 
+  headers: { 
+    "Content-Type": "application/json;charset=utf-8", 
+    "Authorization": "Basic ZjYxMzdjMTQtNTkzNS00MjEwLTg3NTgtZWRlYTllMDNkM2Ri" 
+  }, 
+  body: JSON.stringify(jsonBody), 
+  success: function(httpResponse) { 
+    response.success("sent"); 
+  }, 
+  error: function(httpResponse) { 
+    response.error('Failed with: ' + httpResponse.status); 
+  } 
+});
+});
