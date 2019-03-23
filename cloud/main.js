f0c5d3c7322fb9727af382d5c0b708d2fb20246f
vezzfra <vezzfra@gmail.com>
2019-03-23 09:59:37 +0100

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -3,24 +3,30 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
 
-Parse.Cloud.define('sendNotification', function(req, res) {
-  var jsonBody = {
-    app_id: "f9b9293b-9e2e-451e-bc78-d6ced90b5742",
-    include_player_ids: [req.params.code],
-    contents: {en: "Nuova attivazione"},
-    data: {foo: "bar"}
-  };
+Parse.Cloud.deifne('send', function(request, response) {
 
-  Parse.Cloud.httpRequest({
+var promise = new Parse.Promise();
+
+var jsonBody = { 
+  app_id: "f9b9293b-9e2e-451e-bc78-d6ced90b5742", 
+  include_player_ids: [request.params.code],
+  contents: {en: "Nuova attivazione"}
+};
+
+Parse.Cloud.httpRequest({
     method: "POST",
     url: "https://onesignal.com/api/v1/notifications",
     headers: {
-      "Content-Type": "application/json;charset=utf-8"
+      "Content-Type": "application/json;charset=utf-8",
+      "Authorization": "Basic ZjYxMzdjMTQtNTkzNS00MjEwLTg3NTgtZWRlYTllMDNkM2Ri"
     },
     body: JSON.stringify(jsonBody)
   }).then(function (httpResponse) {
-     res.send(httpResponse);
-  }, function (httpResponse) {
-     res.send(httpResponse);
-  });
+    promise.resolve(httpResponse)
+  },
+  function (httpResponse) {
+    promise.reject(httpResponse);
 });
+
+return promise;
+};
